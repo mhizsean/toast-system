adfc497683fa8adf8bddec143d9892b614479c2b
Esther Adeyi <deyiesther01@gmail.com>
2022-05-06 00:19:16 +0100

    show each status detail and give an update when action has been triggered in the toast

--- src/Content.js
+++ src/Content.js
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { useAsync } from 'react-async';
 import { fetchLikedFormSubmissions } from './service/mockServer';
+import SubmissionToast from './Context/Submission';
+import { Button } from '@mui/material';
 
 export default function Content() {
   const {data, setData, isPending, error, run} = useAsync({
@@ -16,23 +18,33 @@ export default function Content() {
   }, [])
 
 
+  const onLike = submission => {
+    if (data) setData([...data, submission])
+  }
+
   console.log(data, "like")
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
-      {isPending && <div>List loading...</div>}
-
-      {data && (
-          <div>
+      <Typography variant="body1" sx={{ fontStyle: 'italic', fontWeight: 'bold', marginTop: 1 }}>
+        {isPending && <div>loading...</div>}
+        {data && (
+          <>
             {data.map(s => (
-              <div key={s.id}>
-                <Typography variant="body1" sx={{ fontStyle: 'italic', marginTop: 1 }}>{s.data.firstName} {s.data.lastName}</Typography>
-                <span>- {s.data.email} </span>
-              </div>
+              <div key={s.id}> - {s.data.email} |  {s.data.firstName}  </div>
             ))}
+          </>
+        )}
+        {error && (
+          <div>
+            {error.message}
+            <Button onClick={run}>Retry</Button>
           </div>
         )}
+      </Typography>
+
+        <SubmissionToast onLike={onLike} />
     </Box>
   );
 }
--- src/Context/Submission.js
+++ src/Context/Submission.js
@@ -10,10 +10,11 @@ const Toast = ({ onClose, submission, onLike}) => {
         status,
         run: saveSubmission
     } = useAsync({
-        
-    deferFn: args => saveLikedFormSubmission(...args),
-    onResolve: onLike,
+        deferFn: args => saveLikedFormSubmission(...args),
+        onResolve: onLike,
     })
+
+    console.log(status, "ststya")
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway' || isPending) return
@@ -27,84 +28,86 @@ const Toast = ({ onClose, submission, onLike}) => {
       const action = (
         <>
             {['initial', 'rejected'].includes(status) && (
-        <Button
-          onClickCapture={handleLike}
-          color="secondary"
-          size="small"
-          onClick={handleClose}
-        >
-          {status === 'rejected' ? 'RETRY' : 'LIKE'}
-        </Button>
-      )}
+                <Button
+                    onClickCapture={handleLike}
+                    color="secondary"
+                    size="small"
+                    onClick={handleClose}
+                >
+                    {status === 'rejected' ? 'RETRY' : 'LIKE'}
+                </Button>
+            )}
+            
             {isPending ? (
-        <CircularProgress />
-      ) : (
-        <IconButton
-          size="small"
-          aria-label="close"
-          color="inherit"
-          onClick={handleClose}
-        >
-          <CloseIcon fontSize="small" />
-        </IconButton>
-      )}
+                <CircularProgress />
+            ) : (
+                <IconButton
+                    size="small"
+                    aria-label="close"
+                    color="inherit"
+                    onClick={handleClose}
+                >
+                    <CloseIcon fontSize="small" />
+                </IconButton>
+            )}
         </>
       )
 
-
-      const message = (
+    const message = (
         <>
-          <div>{submission.data.email}</div>
-          <div>{submission.data.firstName}</div>
-          <div>{status}</div>
+            <div>{submission.data.email}</div>
+            <div>{submission.data.firstName}</div>
+            <div>
+                {status === "pending" && <>Loading</>}
+                {status === "fulfilled" && <>Liked!</>}
+                {status === "initial" && <>Like!</>}
+                {status === "rejected" && <>Oops! Retry!</>}
+            
+             </div>
         </>
-      )
+    )
 
-      return (
+    return (
         <Snackbar
-        open
-        autoHideDuration={isPending ? null : 5000}
-        key={submission.id}
-        message={message}
-        onClose={handleClose}
-        action={action}
-      />
-      )
+            open
+            autoHideDuration={isPending ? null : 5000}
+            key={submission.id}
+            message={message}
+            onClose={handleClose}
+            action={action}
+        />
+    )
 }
 
-const SubmissionToast = ({onLike}) => {
+const SubmissionToast = ({ onLike }) => {
     const submissions = useRef([])
     const [currentSubmission, setCurrent] = useState(null)
 
-  useEffect(() => {
-    onMessage(data => {
-        setCurrent(current => {
-          if (current) {
-            submissions.current.push(data)
-          }
-          return current ? current : data
+    useEffect(() => {
+        onMessage(data => {
+            setCurrent(current => {
+            if (current) {
+                submissions.current.push(data)
+            }
+            return current ? current : data
+            })
         })
-      })
-  }, [])
+    }, [])
 
-  const handleClose = () => {
-    setCurrent(submissions.current.shift() || null)
-  }
+    const handleClose = () => {
+        setCurrent(submissions.current.shift() || null)
+    }
 
- 
-
-  return (
-    currentSubmission && (
-        <Toast 
-        onLike={onLike}
-        key={currentSubmission.id}
-        submission={currentSubmission}
-        onClose={handleClose}
-        />
+    return (
+        currentSubmission && (
+            <Toast 
+            onLike={onLike}
+            key={currentSubmission.id}
+            submission={currentSubmission}
+            onClose={handleClose}
+            />
+        )
     )
-  
- )
-  
 }
 
 export default SubmissionToast
